628a02202f0ea0556d4ec912dc1a80ae24167343
ayush-that <ayush1337@hotmail.com>
2025-02-23 16:15:21 +0530

    random fix

---
+++ src/env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_SUPABASE_URL: string;
+  VITE_SUPABASE_ANON_KEY: string;
+  VITE_APP_URL: string;
+  VITE_PROD_URL: string;
+  VITE_OPENAI_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+} 
